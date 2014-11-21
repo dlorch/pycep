cd16e0518648d3583a13d559b8e8da062a4659fa
Daniel Lorch <dlorch@gmail.com>
2014-11-21 15:39:00 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "pycep",
+    version = "1.0",
+    author = "Daniel Lorch",
+    author_email = "dlorch@gmail.com",
+    description = "Python Inception",
+    url = "http://github.com/dlorch/pycep/",
+    classifiers = [
+        "Development Status :: 1 - Planning",
+    ],
+)
