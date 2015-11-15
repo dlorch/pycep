76df01831be5480ec097fc1a9255cc0d5550eb31
Daniel Lorch <dlorch@gmail.com>
2015-11-15 22:36:25 +0000

    Add test suite

---
+++ pycep/tests/test_interpreter.py
@@ -0,0 +1,37 @@
+import unittest
+from os import path
+
+class TestInterpreter(unittest.TestCase):
+    
+    def test_beer(self):
+        self.fail("Interpreter missing")
+        
+    def test_classes(self):
+        self.fail("Interpreter missing")
+
+    def test_cliexception(self):
+        self.fail("Interpreter missing")
+
+    def test_friends(self):
+        self.fail("Interpreter missing")
+
+    def test_fib(self):
+        self.fail("Interpreter missing")
+
+    def test_functions(self):
+        self.fail("Interpreter missing")
+
+    def test_helloworld(self):
+        self.fail("Interpreter missing")
+
+    def test_parentsbabies(self):
+        self.fail("Interpreter missing")
+
+    def test_primes(self):
+        self.fail("Interpreter missing")
+
+    def test_queens(self):
+        self.fail("Interpreter missing")
+
+    def test_regex(self):
+        self.fail("Interpreter missing")
