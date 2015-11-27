e91b9ba79c2ef5b43d0fec62a894e3b800aa3c4b
Daniel Lorch <dlorch@gmail.com>
2015-11-27 21:49:15 +0000

    Add Parser Tests

---
+++ pycep/tests/test_parser.py
@@ -0,0 +1,13 @@
+import unittest
+from os import path
+import parser
+import pycep.parser
+
+SAMPLE_PROGRAMS = path.abspath(path.join(path.dirname(__file__), "programs"))
+
+class TestParser(unittest.TestCase):
+
+    def test_helloworld(self):
+        source = open(path.join(SAMPLE_PROGRAMS, "helloworld.py")).read()
+        self.assertEquals(parser.suite(source).totuple(),
+            pycep.parser.suite(source).totuple())
