8b54c97af625ac1aee1bd04d875723c99d01d906
Daniel Lorch <dlorch@gmail.com>
2015-12-06 09:38:08 +0000

    Successfully analyzing hello world example

--- pycep/analyzer.py
+++ pycep/analyzer.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 import ast
+import token
+import symbol
 import pycep.parser
 
 def parse(source):
@@ -40,12 +42,86 @@ def parse(source):
         * Python Abstract Grammar: https://docs.python.org/2/library/ast.html
         * Green Tree Snakes - the missing Python AST docs: https://greentreesnakes.readthedocs.org/
     """
-    st = pycep.parser.suite(source)
-
-    tree = _parse(st)
-    ast.fix_missing_locations(tree)
+    parse_tree = pycep.parser.suite(source)
+    tree = _parse(parse_tree.totuple())
+    ast.fix_missing_locations(tree) # TODO
 
     return tree
 
-def _parse(st):
-    return ast.Module(body=[ast.Print(dest=None, values=[ast.Str("Hello, world!")], nl=True)])
+def _parse(parse_tree):
+    key, values = parse_tree[0], parse_tree[1:]
+    
+    if key == symbol.file_input:
+        node = ast.Module()
+        node.body = []
+        node.body.append(_parse(values[0])) # TODO
+        return node
+    elif key == symbol.stmt:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.simple_stmt:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.small_stmt:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.print_stmt:
+        node = ast.Print()
+        node.dest = None # TODO
+        node.values = [_parse(values[1])] # TODO
+        node.nl = True # TODO
+        return node
+    elif key == symbol.test:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.or_test:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.and_test:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.not_test:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.comparison:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.expr:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.xor_expr:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.and_expr:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.shift_expr:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.arith_expr:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.term:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.factor:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.power:
+        # TODO
+        return _parse(values[0])
+    elif key == symbol.atom:
+        # TODO
+        return _parse(values[0])
+    elif key == token.STRING:
+        node = ast.Str(values[0][1:-1].decode("string-escape"))
+        return node
+    else:
+        if key in token.tok_name:
+            tok_name = token.tok_name[key]
+        elif key in symbol.sym_name:
+            tok_name = symbol.sym_name[key]
+        else:
+            tok_name = None
+
+        raise NotImplementedError(tok_name)
