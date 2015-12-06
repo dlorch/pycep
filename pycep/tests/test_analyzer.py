16c18a149d90c154c65e275312404ad7eb4bb05b
Daniel Lorch <dlorch@gmail.com>
2015-12-06 09:08:35 +0000

    Cleanup ast comparison

--- pycep/tests/test_analyzer.py
+++ pycep/tests/test_analyzer.py
@@ -17,15 +17,22 @@ class TestAnalyzer(unittest.TestCase):
     
     def assertAstEqual(self, first, second, msg=None):
         def astShallowEqual(first, second):
-            """ Shallow equality, don't recurse into sub-items """
+            """ Only check shallow equality - don't recurse into sub-items """
             for (fieldname, value) in ast.iter_fields(first):
-                value2 = getattr(second, fieldname, None)
+                if not hasattr(second, fieldname):
+                    return False
+                value2 = getattr(second, fieldname)
+
+                if type(value) != type(value2):
+                    return False
+                
                 if isinstance(value, list):
                     if len(value) != len(value2):
                         return False
                 else:
                     if value != value2:
                         return False
+            
             return True
         
         if type(first) != type(second):
@@ -35,7 +42,9 @@ class TestAnalyzer(unittest.TestCase):
                 self.fail("%s != %s" % (ast.dump(first), ast.dump(second)))
             else:
                 for (fieldname, value) in ast.iter_fields(first):
-                    value2 = getattr(second, fieldname, None)
+                    # given shallow equality, we know first and second have the same
+                    # attributes, and list attributes are of the same length
+                    value2 = getattr(second, fieldname)
                     if isinstance(value, list):
                         for idx, v in enumerate(value):
                             v2 = value2[idx]
