ebd3f6b3eb8934de9908ed60e849bf88eb208498
Daniel Lorch <dlorch@gmail.com>
2015-12-23 10:55:51 +0000

    Formatting

--- pycep/tokenizer.py
+++ pycep/tokenizer.py
@@ -11,8 +11,8 @@ def generate_tokens(readline):
     [(1, 'print', (1, 0), (1, 5), 'print "Hello, world!"'), (3, '"Hello, world!"', (1, 6), (1, 19), 'print "Hello, world!"'), (0, '', (2, 0), (2, 0), '')]
 
     See also:
-        * Python: Myths about Indentation: http://www.secnetix.de/olli/Python/block_indentation.hawk
         * Python Language Reference: Lexical Analysis: https://docs.python.org/2/reference/lexical_analysis.html
+        * Python: Myths about Indentation: http://www.secnetix.de/olli/Python/block_indentation.hawk
         * Python is not context free: http://trevorjim.com/python-is-not-context-free/
     """
     
