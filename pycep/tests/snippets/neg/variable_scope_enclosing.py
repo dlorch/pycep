5b84505d7def9e5896875ac711a8adf6c9a72131
Daniel Lorch <dlorch@gmail.com>
2015-12-24 08:38:21 +0000

    Better example for modifying enclosing variables

--- pycep/tests/snippets/neg/variable_scope_enclosing.py
+++ pycep/tests/snippets/neg/variable_scope_enclosing.py
@@ -1,10 +1,20 @@
+# If a function only reads from a variable, it's assumed to be global. If the
+# function writes to it ever, it's assumed to be local. In the second function,
+# a is written to, so it's assumed to be local.
+#
 # Code in a nested function's body may access (but not rebind) local variables
 # of an outer function, also known as free variables of the nested function.
 
-def a():
-    n = 12
-    def b():
-        n = n + 1
-    b()
- 
-a()
+a = 123
+def f():
+    print a
+
+f() # prints 123
+
+b = 123
+def g():
+    print b
+    b = 456
+    print b
+
+g() # UnboundLocalError: local variable 'b' referenced before assignment
