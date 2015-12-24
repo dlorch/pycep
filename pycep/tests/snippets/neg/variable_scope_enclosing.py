2e0528a21ae298086d11790f75d1d8580ebe5e81
Daniel Lorch <dlorch@gmail.com>
2015-12-24 08:32:28 +0000

    Rebinding behavior

--- pycep/tests/snippets/neg/variable_scope_enclosing.py
+++ pycep/tests/snippets/neg/variable_scope_enclosing.py
@@ -1,4 +1,5 @@
-# UnboundLocalError: local variable 'n' referenced before assignment
+# Code in a nested function's body may access (but not rebind) local variables
+# of an outer function, also known as free variables of the nested function.
 
 def a():
     n = 12
